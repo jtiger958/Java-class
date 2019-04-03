8b5eb80044adde2b62a9c0926e740f01f603b540
Jang, Wonbeom <40621030+jtiger958@users.noreply.github.com>
2019-04-03 19:08:52 +0900

    Update FriendListFile.java

--- project3/src/FriendListFile.java
+++ project3/src/FriendListFile.java
@@ -4,16 +4,22 @@ public class FriendListFile {
     public FriendList readFileToList(String fileName){
         FriendList friendList = new FriendList();
         try {
-            File file = new File("./data");
+            File file = new File(fileName);
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = "";
             String[] userInfo;
             for (int i = 0; (line = bufferedReader.readLine()) != null; i++){
-                    userInfo = line.split(":");
-                String name, int group, String email, String phone, String profilePicture
-                    friendList.setFriend()
-
+                    userInfo = line.split(": ");
+                    if (userInfo[0].substring(0,2).equals("//")) {
+                        i--;
+                        continue;
+                    }
+                    if (userInfo.length != 5) {
+                        System.out.println("Not enough information in line");
+                        System.out.println("[err] in " + line);
+                    }
+                    friendList.setFriend(i, name, group, email, phone, profilePicture);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
