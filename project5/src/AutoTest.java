6d08a961ddbf0b4ad03bc41554aca6406470fad7
Jang Wonbeom <jtiger958@gmail.com>
2019-06-09 16:10:15 +0900

    edit AutoTest

--- project5/src/AutoTest.java
+++ project5/src/AutoTest.java
@@ -1,9 +1,7 @@
-import aiel.friendlist.Layout.AddFriendListFrame.AddFriendFrame;
 import aiel.friendlist.Layout.FriendListFrame.FriendListFrame;
 
 public class AutoTest {
     public static void main(String[] args) {
         new FriendListFrame("friendlist-norm.data");
-        new AddFriendFrame();
     }
 }
